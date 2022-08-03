18e35c0f0817b3bef501278a57510f984a80c755
Orange <ddeme@ORANGEBEECEE>
2022-08-03 17:06:27 -0400

    Add project files.

---
+++ Contactbutnotreally/Contact.cs
@@ -0,0 +1,11 @@
+﻿
+    public class Contact
+    {
+        public Guid id = new Guid();
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+
